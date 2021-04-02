730e3afa188c933c063cb7d5769a055f88b467c0
Sinthiya <sinthiya.balraj@abbott.com>
2021-04-02 11:32:33 +0530

    Initial commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Sinthiya Balraj on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ TestAppTests/TestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppTests.swift
+//  TestAppTests
+//
+//  Created by Sinthiya Balraj on 02/04/21.
+//
+
+import XCTest
+
+class TestAppTests: XCTestCase {
+
+    func testExample1() {
+        let logger = Logger()
+        logger.logMessage(to: "Hello Sh")
+    }
+
+}
